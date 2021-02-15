5663709b048b66c7573c0df538f68b4722011e40
Jacob <jacob.m.mcfaul@gmail.com>
2021-02-15 13:17:29 -0700

    More to come removed

--- portfolio/src/components/ProjectData.jsx
+++ portfolio/src/components/ProjectData.jsx
@@ -4,6 +4,5 @@ import ComingSoon from '../img/coming-soon.png'
 
 export const ProjectData = [
     { name: "Floors Plus", img: Floor, url: "https://floorsplus.netlify.app/", github: "https://github.com/McTrip777/FloorsPlus" },
-    { name: "Jacob's Pizza", img: Pizza, url: "https://jacobspizzasite.netlify.app/", github: "https://github.com/McTrip777/PizzaSite" },
-    { name: "More To Come", img: ComingSoon, url: null, github: null }
+    { name: "Jacob's Pizza", img: Pizza, url: "https://jacobspizzasite.netlify.app/", github: "https://github.com/McTrip777/PizzaSite" }
 ]
