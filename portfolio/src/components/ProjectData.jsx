da4ad56013954ade84a0ba00fab7092b3f2482d7
Jacob <jacob.m.mcfaul@gmail.com>
2021-04-05 13:19:30 -0600

    Added another project

--- portfolio/src/components/ProjectData.jsx
+++ portfolio/src/components/ProjectData.jsx
@@ -1,7 +1,9 @@
 import Floor from '../img/floor-installation.jpg'
 import Pizza from '../img/pizza.jpg'
+import Twraps from '../img/twraps.png'
 
 export const ProjectData = [
     { name: "Floors Plus", img: Floor, url: "https://floorsplus.netlify.app/", github: "https://github.com/McTrip777/FloorsPlus" },
-    { name: "Jacob's Pizza", img: Pizza, url: "https://jacobspizzasite.netlify.app/", github: "https://github.com/McTrip777/PizzaSite" }
+    { name: "Jacob's Pizza", img: Pizza, url: "https://jacobspizzasite.netlify.app/", github: "https://github.com/McTrip777/PizzaSite" },
+    { name: "T-Wraps", img: Twraps, url: "https://t-wraps.netlify.app/", github: "https://github.com/McTrip777/Twraps" }
 ]
