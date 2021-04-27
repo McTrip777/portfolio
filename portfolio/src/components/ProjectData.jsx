6f3c006844ee8e6ceba85808b8bc94a1c1c714d8
Jacob <jacob.m.mcfaul@gmail.com>
2021-04-27 12:25:16 -0600

    New project

--- portfolio/src/components/ProjectData.jsx
+++ portfolio/src/components/ProjectData.jsx
@@ -1,9 +1,11 @@
 import Floor from '../img/floor-installation.jpg'
 import Pizza from '../img/pizza.jpg'
 import Twraps from '../img/twraps.png'
+import AlgoImg from '../img/algoImg.jpg'
 
 export const ProjectData = [
     { name: "Floors Plus", img: Floor, url: "https://floorsplus.netlify.app/", github: "https://github.com/McTrip777/FloorsPlus" },
     { name: "Jacob's Pizza", img: Pizza, url: "https://jacobspizzasite.netlify.app/", github: "https://github.com/McTrip777/PizzaSite" },
-    { name: "T-Wraps", img: Twraps, url: "https://t-wraps.netlify.app/", github: "https://github.com/McTrip777/Twraps" }
+    { name: "T-Wraps", img: Twraps, url: "https://t-wraps.netlify.app/", github: "https://github.com/McTrip777/Twraps" },
+    { name: "Algos", img: AlgoImg, url: "https://all-sorting-algorithms.netlify.app/", github: "https://github.com/McTrip777/AllSortingAlgos" }
 ]
